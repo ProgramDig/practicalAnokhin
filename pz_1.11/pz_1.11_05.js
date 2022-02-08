a5604117e1892ad6de6f62303db7bc47045c3040
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-02-08 13:12:58 +0300

    pz_1.11_05_01

--- pz_1.11/pz_1.11_05.js
+++ pz_1.11/pz_1.11_05.js
@@ -6,7 +6,30 @@ const isPrimeNumber = (number) => {
 			return false;
 		}
 	}
-	return number > 1;
+	return true;
 };
+const isPositive = (number) => (number > 0)? true: false;
+const isMul = (number) => (number % (2, 5, 3, 6, 9) === 0)? true: false; 
+const conditionNumber = (number, isMul, isPosit, isPrime) =>{
+	let result = `Number ${number}:`;
+	if (isMul (number)) {
+		result += ('\nDivided by 2, 5, 3, 6, 9 without remainder.');
+	} else {
+		result += ("\nDivided't by 2, 5, 3, 6, 9 without remainder.")
+	}
+	if (isPrime (number)) {
+		result += ('\nIs a prime number.');
+	} else {
+		result += ("\nIs't a prime number.");
+	}
+	if (isPosit (number)) {
+		result += ('\nIs a positive number.');
+	} else {
+		result += ("\nIs't a positive number.")
+	}
+	return result
+}
 
-console.log(isPrimeNumber(7));
+const str = conditionNumber(90,isMul,isPositive,isPrimeNumber);
+
+console.log(str);
