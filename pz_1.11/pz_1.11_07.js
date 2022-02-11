327b4956f488585afa26c8f360f61308a5ba0dc5
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-02-11 10:36:50 +0300

    com

--- pz_1.11/pz_1.11_07.js
+++ pz_1.11/pz_1.11_07.js
@@ -2,17 +2,18 @@ let array = [];
 
 const min = -10;
 const max = 20;
+const num = 5;
 
-for(let i = 0; i < 5; i++){
+for(let i = 0; i < num; i++){
 	array[i] = new Array();
-	for(let j = 0; j < 5; j++){
+	for(let j = 0; j < num; j++){
 		array[i][j] = Math.floor(Math.random() * max + min);
 	}
 }
 
 
- for(let i = 0; i < 5; i++){
- 	for(let j = 0; j < 5; j++){
+ for(let i = 0; i < num; i++){
+ 	for(let j = 0; j < num; j++){
  		if(i === j){
  			if (array[i][j] > 0){
  				array[i][j] = 1;
