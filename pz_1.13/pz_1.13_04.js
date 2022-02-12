59a5e91f41c809072c7d2b29d87e60484f070d08
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-02-12 11:30:21 +0300

    pz_1.13_05

---
+++ pz_1.13/pz_1.13_04.js
@@ -0,0 +1,8 @@
+// function createGreetable (str) {
+// 	const result = str;
+// 	result.greet = function(greeting) {return `${greeting}, ${this}!`};
+// 	return result;
+// }
+
+// const g = createGreetable('Oleg');
+// console.log (g.greet('Hello'));
---
+++ pz_1.13/pz_1.13_05.js
@@ -0,0 +1,29 @@
+class Generator {
+	// value;
+	// upValue;
+	// isFirst
+	constructor () {
+		this.value = 0;
+		this.upValue = 0;
+		this.isFirst = true;
+	}
+	generator () {
+		if (this.isFirst === false){
+			this.value += this.upValue;
+		}
+		return this.value; 
+	}
+	sequence (start , step) {
+		if (this.isFirst) {
+			this.value = start;
+			this.upValue = step;
+			this.isFirst = false;
+		} 
+		return this.generator();
+	}
+}
+let b = new Generator();
+b.sequence(10,5)
+console.log(b.value);
+console.log(b.generator());
+console.log(b.generator());
